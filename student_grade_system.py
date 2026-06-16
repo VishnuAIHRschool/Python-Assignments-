0f3ecc27b7443242bed97b2c504a5fc65af111b4
VishnuAIHRSchool <vishnutalenthr@gmail.com>
2026-06-16 16:30:43 +0530

    Added Student Grade System assignment

---
+++ student_grade_system.py
@@ -0,0 +1,24 @@
+# Student Grade System
+
+student_name = input("Enter student name: ")
+
+marks = int(input("Enter student marks: "))
+
+if 90 <= marks <= 100:
+    grade = "A"
+
+elif 80 <= marks <= 89:
+    grade = "B"
+
+elif 70 <= marks <= 79:
+    grade = "C"
+
+elif 60 <= marks <= 69:
+    grade = "D"
+
+else:
+    grade = "F"
+
+print("\nStudent Name:", student_name)
+print("Marks:", marks)
+print("Grade:", grade)
